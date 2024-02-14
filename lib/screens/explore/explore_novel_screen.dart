425d2dc66906edaaa89114150060b1bfdb3c6a34
jhiven <jivenagnar11@gmail.com>
2024-02-14 21:32:11 +0700

    [fix] fix merge fc1935d issue

--- lib/screens/explore/explore_novel_screen.dart
+++ lib/screens/explore/explore_novel_screen.dart
@@ -1,5 +1,5 @@
 import 'package:animo/models/base_data.dart';
-import 'package:animo/screens/explore/explore_media_future.dart';
+import 'package:animo/screens/explore/feed_view.dart';
 import 'package:flutter/material.dart';
 
 class ExploreNovelScreen extends StatefulWidget {
@@ -11,24 +11,14 @@ class ExploreNovelScreen extends StatefulWidget {
 
 class _ExploreNovelScreenState extends State<ExploreNovelScreen>
     with AutomaticKeepAliveClientMixin {
-  final List<Map<String, String>> options = [
-    {'srel': 'Most popular'},
-    {'sfrel': 'Highest rating'},
-    {'srank': 'Most follows'},
-    {'srate': 'Newest'},
-    {'sreview': 'Most views'},
-    {'sdate': 'Last updated'},
-  ];
-
   @override
   bool get wantKeepAlive => true;
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return ExploreMediaFuture(
+    return const FeedView(
       mediaType: MediaType.novel,
-      options: options,
     );
   }
 }
